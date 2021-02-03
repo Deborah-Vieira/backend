329ef7ef912bcd751c2f4ab617b1dd5eed02e972
Deborah Vieira <deborahanalistati@gmail.com>
2021-02-03 16:00:07 -0300

    List-Account-And-Validation-Age

--- Sistema-Bancario/src/index.ts
+++ Sistema-Bancario/src/index.ts
@@ -2,13 +2,14 @@
 import express, {Request, Response} from 'express';
 import cors from 'cors';
 import {accounts} from './accounts'
+import { NumericLiteral } from 'typescript';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-
+//create Account
 app.post("/users/create", (req: Request, res: Response) =>{
     try{
         const {name, CPF, dateOfBirth} = req.body;
@@ -16,6 +17,15 @@ app.post("/users/create", (req: Request, res: Response) =>{
         const [day, month, year] = dateOfBirth.split("/")
 
         const dateBirth : Date = new Date(`${year}-${month}-${day}`)
+
+        //validation age > 18
+        const ageMilisseconds : number = Date.now() - dateBirth.getTime()
+        const ageInYears : number = ageMilisseconds / 1000 / 60 / 60 / 24 / 365
+
+            if(ageInYears < 18 ){
+                res.statusCode = 404
+                throw new Error("Age must be over 18 years");
+            }
 
         accounts.push({
             name,
@@ -28,7 +38,22 @@ app.post("/users/create", (req: Request, res: Response) =>{
         res.status(200).send({message: "User created successfully"});
 
     }catch(error){
-        res.status(400).send(error.message);
+        res.send(error.message);
+    }
+})
+
+//List Account
+app.get("/users/all", (req: Request, res: Response) =>{
+    try{
+        if(!accounts.length){
+            res.statusCode = 404
+            throw new Error("No account found");
+            
+        }       
+        res.status(200).send(accounts);
+
+    }catch(error){
+        res.send(error.message);
     }
 })
 
